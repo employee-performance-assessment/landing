cf7f3e538cc1a54e19f058ca7ab1695efc185be6
Перевалов Антон <paspean@mail.ru>
2024-06-13 22:53:06 +0700

    начало лендинга

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import './Header.css'
+const Header = () => {
+    return <header className="header">
+        <div className="header__pouring">WellDone</div>
+        <p className="header__description">Соберите все данные о ваших сотрудниках в одном месте и постройте эффективную команду</p><p className="header__link">Перейти на сайт</p>
+    </header>
+}
+
+export default Header
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import './Main.css'
+const Main = () => {
+    return <main className="main">
+        <div className='place'>
+            <div className="place__title">Вы попали в нужное место, если</div>
+            <p className="place__description">Не понятно, сколько ваш сотрудник выполняет задач и какую ценность он приносит компании?</p><p className="place__description place__description_yellow">Сложно отследить, насколько эффективно он работает в команде и какие у него навыки общения.</p>
+        </div>
+        
+    </main>
+}
+
+export default Main
